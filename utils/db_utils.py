8f69c8087c673be517505c7ff47f66edaaea1b81
daniellpsilva1 <a104086@alunos.uminho.pt>
2025-02-08 21:47:59 +0000

    Add .gitignore and update db_utils.py with environment configuration

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,11 +1,27 @@
 import streamlit as st
 import pymongo
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file if running locally
+if not hasattr(st, 'secrets'):
+    load_dotenv()
 
 @st.cache_resource
 def init_connection():
+    # Use st.secrets if running in cloud, otherwise use environment variables
+    if hasattr(st, 'secrets'):
+        username = st.secrets.mongo.username
+        password = st.secrets.mongo.password
+        cluster = st.secrets.mongo.cluster
+    else:
+        username = os.getenv('MONGO_USERNAME')
+        password = os.getenv('MONGO_PASSWORD')
+        cluster = os.getenv('MONGO_CLUSTER')
+    
     return pymongo.MongoClient(
-        "mongodb+srv://DanielSilva:danielsilva@garminfakedata.qppxh.mongodb.net/?retryWrites=true&w=majority&appName=GarminFakeData"
+        f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority&appName=GarminFakeData"
     )
 
 @st.cache_data
